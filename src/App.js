6a671bbce64a2eb440c0fac83c4af7a886aad8eb
DanielTorresV <aldodaniel.tv@gmail.com>
2024-04-09 21:26:14 -0700

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Dashboard from "views/pages/Dashboard/Dashboard";
+
+const style = {
+  bg: "h-screen w-screen p-4 bg-gradient-to-r from-[#171717] to-[#3d3d3d]",
+};
 
 function App() {
   return (
-    <div>
+    <div className={style.bg}>
+      <Dashboard />
     </div>
   );
 }
